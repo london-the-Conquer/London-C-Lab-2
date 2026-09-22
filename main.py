c0d67fb3794fee885756a6979cddefc557599bb3
london-the-Conquer <lchesnut29@setoncatholic.org>
2026-09-22 20:12:57 +0000

    commit

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-x = 6
-if x > 5:
-    print("hello")
-    elif x > 5
-    print("Goodybye")
+import random
+
+
+game_number = random.randint(1,10) 
+#print(game_number )
+while(True):
+
+    guess = int(input("Guess a number between 1 and 10"))
+
+    if guess > game_number:
+        print("Go Lower")
+    elif guess < game_number:
+        print("Go Higher")
+    else:
+        print("You win!")
+        break
